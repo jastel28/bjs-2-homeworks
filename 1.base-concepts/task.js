9a31d45b3585db51663103d09697d8736802729f
Alexey Veretennikov <jastel28@gmail.com>
2023-04-20 22:45:51 +0500

    Первое задание готово, по второму есть вопрос

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if (d > 0) {
+    let firstSolution = (-b + Math.sqrt(d) )/(2*a);
+    let secondSolution = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(firstSolution, secondSolution);
+  } else if (d == 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else if (d < 0) {
+    arr.push();
+  }
+  
   
   return arr;
 }
 
+
+
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    let arr = [];
+
+    let p = (percent / 100) / 12;
+    let mainDebt = amount - contribution;
+    let mounthPayment = mainDebt * (p + (p / (((1 + p)**countMonths) - 1)));
+    let totalMount = (mounthPayment * countMonths).toFixed(2);
+
+    arr.push(parseFloat(totalMount));
+
+    return arr;
 }
