259de8c68add152ebf1994da13f15c66f6af2352
Alexey Veretennikov <jastel28@gmail.com>
2023-04-26 23:57:14 +0500

    Сделал первое задание, есть вопрос по второму.

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,14 +1,48 @@
 function getArrayParams(...arr) {
 
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = null;
+
+  for(let i = 0; i < arr.length; i++) {
+
+    sum += arr[i];
+
+    if (arr[i] > max) {
+      max = arr[i]; 
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  let sumString = (sum / arr.length).toFixed(2);
+  let avg = parseFloat(sumString);
+
+
+
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
 
+  if (arr.length == 0) {
+    return 0; 
+  } else {
+    for(let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+  
+  
+
 }
-
 function differenceMaxMinWorker(...arr) {
-
+  let max = Math.max.apply(null, ...arr);
+  let min = Math.min.apply(null, ...arr);
+  let dif = max - min;
+  return dif; 
 }
 
 function differenceEvenOddWorker(...arr) {
